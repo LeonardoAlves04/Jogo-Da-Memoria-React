4a9d6ea8550059e3d9f2bcd0a4eb343d12290ba9
Leonardo Alves <leonardoalves201510@gmail.com>
2022-11-07 13:57:55 -0300

    arrumando imagens

--- src/components/CardElement.js
+++ src/components/CardElement.js
@@ -5,7 +5,7 @@ export default function CardElement(props) {
       <div onClick={() => { props.handleFlip(props.card) }} id={props.card.id} className={`card ${props.card.flipped ? "flip" : ""}`}>
           <div className="card_front">
               <img className="icon"
-                  src={`assets/images/${props.card.icon}.png`}
+                  src={`./public/assets/images/${props.card.icon}.png`}
                   alt={props.card.icon}></img>
           </div>
           <div className="card_back">
